34370f11de7b4404ed69d179600eebbb45450893
KARTHIK <KARTHIK@User>
2018-12-22 12:47:13 +0530

    gh

---
+++ src/sel/Script.java
@@ -0,0 +1,19 @@
+package sel;
+
+import org.testng.annotations.Test;
+
+public class Script {
+	@Test
+	public void a()
+	{
+		System.out.println("i am a ");
+	}
+	@Test
+	public void b()
+	{
+		System.out.println("i am b ");
+	}
+	
+	
+
+}
